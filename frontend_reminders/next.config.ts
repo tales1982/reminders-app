fa555f706bd5e6f59a5a38d594fa4f51db5c01ae
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-04-23 10:25:16 +0200

    arrumando o foot na pagina inicial

--- frontend_reminders/next.config.ts
+++ frontend_reminders/next.config.ts
@@ -4,6 +4,9 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // habilita suporte a SSR e nomes de classes
+  },
 };
 
 const withPWAModule = withPWA({
